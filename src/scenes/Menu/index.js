787f38ebdcba6537ffe2b05964b1944803162a6b
mikolajspeichert <mikolaj.speichert@gmail.com>
2018-05-21 19:38:24 +0200

    Add settings, fullscreen toggle

--- src/scenes/Menu/index.js
+++ src/scenes/Menu/index.js
@@ -1,13 +1,49 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { compose, withState, withHandlers } from 'recompose'
+
 import { MenuItem, MenuContainer, MenuTitle } from './styles'
 
-const Menu = ({ onPlay }) => (
-  <MenuContainer>
-    <MenuTitle>PR TANKS</MenuTitle>
-    <MenuItem onClick={onPlay}>Play</MenuItem>
-    <MenuItem onClick={() => {}}>Settings</MenuItem>
-  </MenuContainer>
+const enhance = compose(
+  withState('showingSettings', 'setSettings', false),
+  withState('fullscreen', 'setFullscreen', 'off'),
+  withHandlers({
+    onSettings: ({ setSettings, showingSettings }) => () =>
+      setSettings(!showingSettings),
+    onFullscreen: ({ fullscreen, setFullscreen }) => () => {
+      if (fullscreen === 'off') {
+        document.getElementById('root').webkitRequestFullScreen &&
+          document.getElementById('root').webkitRequestFullScreen()
+        setFullscreen('on')
+      } else {
+        setFullscreen('off')
+        document.webkitCancelFullScreen()
+      }
+    },
+  })
+)
+const Menu = enhance(
+  ({ onPlay, onSettings, showingSettings, fullscreen, onFullscreen }) => {
+    let content
+    if (showingSettings) {
+      content = (
+        <MenuContainer>
+          <MenuTitle>Settings</MenuTitle>
+          <MenuItem onClick={onFullscreen}>Fullscreen: {fullscreen}</MenuItem>
+          <MenuItem onClick={onSettings}>Back</MenuItem>
+        </MenuContainer>
+      )
+    } else {
+      content = (
+        <MenuContainer>
+          <MenuTitle>PR TANKS</MenuTitle>
+          <MenuItem onClick={onPlay}>Play</MenuItem>
+          <MenuItem onClick={onSettings}>Settings</MenuItem>
+        </MenuContainer>
+      )
+    }
+    return content
+  }
 )
 
 Menu.propTypes = {
